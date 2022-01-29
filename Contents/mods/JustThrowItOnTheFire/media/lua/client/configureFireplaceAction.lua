78f22d4133523dffaf7c5813596edd5795947167
tuliomir <tulio.mir@gmail.com>
2022-01-29 10:53:42 -0300

    adding zombies fuel to fireplace, destroying them

--- Contents/mods/JustThrowItOnTheFire/media/lua/client/configureFireplaceAction.lua
+++ Contents/mods/JustThrowItOnTheFire/media/lua/client/configureFireplaceAction.lua
@@ -1,6 +1,16 @@
 require 'Camping/ISUI/ISCampingMenu'
 require 'Camping/CCampfireSystem'
 
+local function getHumanReadableTime(minutes)
+    local friendlyAmount = ""
+    if minutes > 60 then
+        friendlyAmount = "about " .. math.floor(minutes/60) .. " hours"
+    else
+        friendlyAmount = minutes .. " minutes"
+    end
+    return friendlyAmount
+end
+
 -- This function calculats the amount of fuel in each item inside the container
 -- If the "removeFuelItems" parameter is passed, it means the fuel items must also be removed from container
 local function getContainerFuelAmount(container, removeFuelItems)
@@ -71,13 +81,24 @@ local function addFuelCallback(worldobjects, container, campfire, playerNumber)
     CCampfireSystem.instance:sendCommand(playerObj, 'addFuel', args)
 end
 
-local function kickZombiesIntoFireCallback(worldobjects, zombieInventories, playerNumber)
+local function kickZombiesIntoFireCallback(worldobjects, zombieFuelObj, playerNumber, campfire)
+    -- Remove the zombies from the world, they will be turned into fuel
+    for i,v in ipairs(zombieFuelObj.zombiesInventories) do
+       local zombieInventory = v.inventory
+        local isoDeadBody = zombieInventory:getParent()
 
+        isoDeadBody:removeFromWorld()
+        isoDeadBody:removeFromSquare()
+    end
+
+    -- Convert zombies and their items into campfire fuel
+    local fuelAmt = zombieFuelObj.totalFuel
+    local args = { x = campfire.x, y = campfire.y, z = campfire.z, fuelAmt = fuelAmt }
+    local playerObj = getSpecificPlayer(playerNumber)
+    CCampfireSystem.instance:sendCommand(playerObj, 'addFuel', args)
 end
 
--- This function will be run at Events.OnFillWorldObjectContextMenu , basically at any right button
--- click on the game. It must be fast to identify if it is not needed and return.
-ISWorldObjectContextMenu.AssignExpressFuelingAction = function(playerNumber, context, worldobjects, test)
+local function getCampfireFromWorldObjects(worldobjects)
     local container = nil
 
     -- Iterating all worldobjects to find the one that has a container
@@ -101,7 +122,16 @@ ISWorldObjectContextMenu.AssignExpressFuelingAction = function(playerNumber, con
 
     -- Retrieving the actual Campfire instance
     ISCampingMenu.campfire = CCampfireSystem.instance:getLuaObjectOnSquare(parentIsoObj:getSquare())
-    local campfire = ISCampingMenu.campfire
+    return container, ISCampingMenu.campfire
+end
+
+-- This function will be run at Events.OnFillWorldObjectContextMenu , basically at any right button
+-- click on the game. It must be fast to identify if it is not needed and return.
+ISWorldObjectContextMenu.AssignExpressFuelingAction = function(playerNumber, context, worldobjects, test)
+    local container, campfire = getCampfireFromWorldObjects(worldobjects)
+    if container == nil or campfire == nil then return end
+
+
     local fuelAmount = campfire.fuelAmt
     local fuelToAdd = getContainerFuelAmount(container)
 
@@ -109,19 +139,18 @@ ISWorldObjectContextMenu.AssignExpressFuelingAction = function(playerNumber, con
     if fuelToAdd == 0 then return end
 
     -- Add the "add fuel" option to the Context Menu ( todo: translate this )
-    local friendlyAmount = ""
-    if fuelToAdd > 60 then
-        friendlyAmount = "about " .. math.floor(fuelToAdd/60) .. " hours"
-    else
-        friendlyAmount = fuelToAdd .. " minutes"
-    end
-    -- Add a tooltip with better information about the fuel, improve this label
-    local labelToShow = "Add " .. friendlyAmount .. " of fuel from container"
+    -- TODO: Add a tooltip with better information about the fuel, improve this label
+    local labelToShow = "Add " .. getHumanReadableTime(fuelToAdd) .. " of fuel from container"
     context:addOption(labelToShow, worldobjects, addFuelCallback, container, campfire, playerNumber)
 end
 Events.OnFillWorldObjectContextMenu.Add(ISWorldObjectContextMenu.AssignExpressFuelingAction)
 
 ISWorldObjectContextMenu.AssignKickZombiesIntoCampfireAction = function (playerNumber, context, worldobjects)
+    -- First checking if the target is a campfire
+    local container, campfire = getCampfireFromWorldObjects(worldobjects)
+    if container == nil or campfire == nil then return end
+
+    -- It is a campfire. Are there zombies nearby?
     local zombieFuelObj = getZombieInventoriesAroundPlayer(playerNumber, {})
     local amountOfZombies = zombieFuelObj.zombiesFound
 
@@ -129,8 +158,11 @@ ISWorldObjectContextMenu.AssignKickZombiesIntoCampfireAction = function (playerN
         return -- No zombies found. Do nothing.
     end
 
+    -- Getting amount of fuel from zombies
     local totalFuel = zombieFuelObj.totalFuel
-    local labelToShow = "Found " .. amountOfZombies .. " zombies: " .. totalFuel
-    context:addOption(labelToShow, worldobjects, nil)
+    local labelToShow = "Found " .. amountOfZombies .. " zombies: " .. getHumanReadableTime(totalFuel)
+
+    -- Adding option. TODO: This should be merged with the function above for performance.
+    context:addOption(labelToShow, worldobjects, kickZombiesIntoFireCallback, zombieFuelObj, playerNumber, campfire)
 end
 Events.OnFillWorldObjectContextMenu.Add(ISWorldObjectContextMenu.AssignKickZombiesIntoCampfireAction)
